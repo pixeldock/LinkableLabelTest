9e8075ac5cdbaa9b913ab9b5509e2d76d2c34f99
Jörn Schoppe <joern@pixeldock.com>
2017-12-07 09:05:26 +0100

    Initial Commit

---
+++ LinkableLabelTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinkableLabelTest
+//
+//  Created by Jörn Schoppe on 07.12.17.
+//  Copyright © 2017 Jörn Schoppe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
